15e80eaf4d19756b58cf12b6d00e83ac7b88b4b1
unknown <nayemwd7200@gmail.comgit config --global user.name Abunayem-7200>
2026-03-29 14:56:56 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,17 @@
+// START PORTFOLIO AREA
+var mixer = mixitup('.portfolio-grid');
+
+// MENU
+
+function openMenu() {
+    document.getElementById('ham-menu').style.width = "100%";
+}
+
+function closeMenu() {
+    document.getElementById('ham-menu').style.width = "0";
+}
+
+
+// ANIMATION
+
+new WOW().init();
